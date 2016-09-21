bcce3d89eeb01532e678a5ba133addea747aa1bd
Yehor Yudin <yehor.yudin@gmail.com>
2016-09-21 14:03:34 +0200

    Interface to return acceleration created, function to return latest accelration meaurments created

---
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/AccelerationProvider.java
@@ -0,0 +1,8 @@
+package de.ferienakademie.smartquake.excitation;
+
+/**
+ * Created by user on 21.09.2016.
+ */
+public interface AccelerationProvider {
+    public double[] getAccelaration();
+}
---
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/ExcitationManager.java
@@ -0,0 +1,7 @@
+package de.ferienakademie.smartquake.excitation;
+
+/**
+ * Created by user on 21.09.2016.
+ */
+public class ExcitationManager {
+}
