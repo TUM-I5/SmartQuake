3699cc5edd89ca692207866b1f511319e61eee4b
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2016-09-21 13:28:15 +0200

    Added Beam and Node classes

---
+++ app/src/main/java/de/ferienakademie/smartquake/model/Beam.java
@@ -0,0 +1,51 @@
+package de.ferienakademie.smartquake.model;
+
+/**
+ * Created by yuriy on 21/09/16.
+ */
+public class Beam {
+
+    private Node startNode;
+    private Node endNode;
+
+    private float thickness;
+
+    public Beam(Node startNode, Node endNode, float thickness) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.thickness = thickness;
+    }
+
+    public Beam(Node startNode, Node endNode) {
+        this(startNode, endNode, 10);
+    }
+
+    public Beam(double startX, double startY, double endX, double endY) {
+        this(new Node(startX, startY), new Node(endX, endY));
+    }
+
+    public Node getStartNode() {
+        return startNode;
+    }
+
+    public void setStartNode(Node startNode) {
+        this.startNode = startNode;
+    }
+
+    public Node getEndNode() {
+        return endNode;
+    }
+
+    public void setEndNode(Node endNode) {
+        this.endNode = endNode;
+    }
+
+    public float getThickness() {
+        return thickness;
+    }
+
+    public void setThickness(float thickness) {
+        this.thickness = thickness;
+    }
+
+}
