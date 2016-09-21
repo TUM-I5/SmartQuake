830abfa8a39cd0e5b840a9f709f5ea8ddd0a6fa7
Felix Wechsler <felix.wechsler@tum.de>
2016-09-21 17:52:27 +0200

    startSimulation method declared

--- app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
@@ -1,12 +1,21 @@
 package de.ferienakademie.smartquake.kernel2;
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
+import de.ferienakademie.smartquake.model.Structure;
 
 /**
  * Created by Felix Wechsler on 21/09/16.
  */
 public class TimeIntegration {
 
+    /*
+    * @param k1
+    *          object of type structure to obtain all matrices
+    *
+    **/
+    public void startSimulation(Structure k1){
 
+
+    }
 
 }
