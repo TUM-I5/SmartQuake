f996c85ecbc8e37bc2fad40c1f1c87a4f7c1e257
Felix Wechsler <felix.wechsler@tum.de>
2016-09-21 17:43:00 +0200

    rearranging

--- app/src/main/java/de/ferienakademie/smartquake/kernel2/Euler.java
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/Euler.java
@@ -14,12 +14,15 @@ public class Euler implements TimeIntegrationSolver {
     public void nextStep(DenseMatrix64F x, DenseMatrix64F xDot , DenseMatrix64F xDotDot, double t, double delta_t) {
         //store old (n-1) velocity
         DenseMatrix64F oldxDot = xDot.copy();
+
         //velocity at n
         CommonOps.addEquals(xDot, delta_t, xDotDot);
-        //create average matrix of velocities at n and n+1
+
+        //create average matrix of velocities at step n and n+1
         DenseMatrix64F averageXDot=xDot.copy();
         CommonOps.addEquals(averageXDot,1, oldxDot);
 
+        //displacement at step n+1
         CommonOps.addEquals(x, delta_t, averageXDot);
 
 
