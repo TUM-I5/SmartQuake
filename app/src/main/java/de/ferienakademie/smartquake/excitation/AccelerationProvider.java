8dbfb69e1d0c00f059186cef36527cec1d5c8d70
Yehor Yudin <yehor.yudin@gmail.com>
2016-09-21 14:40:52 +0200

    commnets on accelaration inteface

--- app/src/main/java/de/ferienakademie/smartquake/excitation/AccelerationProvider.java
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/AccelerationProvider.java
@@ -3,6 +3,11 @@ package de.ferienakademie.smartquake.excitation;
 /**
  * Created by user on 21.09.2016.
  */
+
 public interface AccelerationProvider {
+    /**
+     *
+     * @return first element acceleration in X axis, second element acceleration in Y axis
+     */
     public double[] getAccelaration();
 }
