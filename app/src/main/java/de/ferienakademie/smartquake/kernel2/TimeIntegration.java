2cfad511e9d40b628ee48bfa0f6afd3108dd4014
Felix Wechsler <felix.wechsler@tum.de>
2016-09-22 15:29:30 +0200

    prepareSimulation method inserted

--- app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
@@ -11,6 +11,21 @@ public class TimeIntegration {
     Kernel1 kernel1;
     boolean isRunning;
 
+    //total computed time
+    double t;
+    //time_step
+    double delta_t;
+
+    //JUST FOR TESTING DIRECTLY SENSORS INPUT
+    double[] acceleration;
+
+    //matrices of velocity
+    DenseMatrix64F xDot;
+    //matrices of acceleration
+    DenseMatrix64F xDotDot;
+
+    //this solver provides the numerical algorithm  for calculating the displacement
+    TimeIntegrationSolver solver;
 
     /*
     * @param kernel1
@@ -21,46 +36,54 @@ public class TimeIntegration {
         this.kernel1 = kernel1;
     }
 
+
+    public void prepareSimulation(){
+        //stores the global simulation time
+        t = 0;
+
+        solver = new Euler();
+
+        //initial condition for the velocity
+        xDot = new DenseMatrix64F(kernel1.getNumDOF(),1);
+        xDot.zero();
+        for(int j=0; j<kernel1.getNumDOF(); j+=3){
+            xDot.add(j,0, 10);
+        }
+        xDot.add(3,0,20);
+        xDot.add(1,0,40);
+        xDot.add(6,0,-20);
+        xDot.add(10,0,-10);
+        xDot.add(4,0,-10);
+        xDot.add(7,0,-80);
+        xDot.add(12,0,150);
+        xDot.add(13,0,100);
+        xDot.zero();
+        //xDotDot must be calculated by the external load forces and the differnetial equation
+
+        //THIS IS JUST A WORKAROUND/MINIMAL EXAMPLE
+        xDotDot = new DenseMatrix64F(kernel1.getNumDOF(),1);
+        xDotDot.zero();
+
+        //only for fixed stepsize
+        delta_t = 0.0000001;
+    }
+
     public void start() {
         isRunning = true;
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //stores the global simulation time
-                double t = 0;
-                double delta_t;
 
-                //this solver provides the numerical algorith  for calculating the displacement
-                TimeIntegrationSolver solver = new Euler();
-
-                //initial condition for the velocity
-                DenseMatrix64F xDot = new DenseMatrix64F(kernel1.getNumDOF(),1);
-                xDot.zero();
-                for(int j=0; j<kernel1.getNumDOF(); j+=3){
-                    xDot.add(j,0, 10);
-                }
-                xDot.add(3,0,20);
-                xDot.add(1,0,40);
-                xDot.add(6,0,-20);
-                xDot.add(10,0,-10);
-                xDot.add(4,0,-10);
-                xDot.add(7,0,-80);
-                xDot.add(12,0,150);
-                xDot.add(13,0,100);
-                //xDotDot must be calculated by the external load forces and the differnetial equation
-
-                //THIS IS JUST A WORKAROUND/MINIMAL EXAMPLE
-                DenseMatrix64F xDotDot = new DenseMatrix64F(kernel1.getNumDOF(),1);
-                xDotDot.zero();
-
-                //only for fixed stepsize
-                delta_t = 0.0000001;
+                prepareSimulation();
                 while(isRunning) {
                     //calculate new position
                     solver.nextStep(kernel1.getDisplacementVector(), xDot, xDotDot,t, delta_t);
+
+                    acceleration=kernel1.getAccelerationProvider().getAcceleration();
                     for(int j=0; j<kernel1.getNumDOF(); j+=3){
-                        xDotDot.add(j,0, -5*xDot.get(j,0)-10*kernel1.getDisplacementVector().get(j, 0));
-                        xDotDot.add(j+1,0, -5*xDot.get(j+1,0)-10*kernel1.getDisplacementVector().get(j+1, 0));
+                        //xDotDot.add(j,0, -5*xDot.get(j,0)-10*kernel1.getDisplacementVector().get(j, 0));
+                        //xDotDot.add(j+1,0, -5*xDot.get(j+1,0)-10*kernel1.getDisplacementVector().get(j+1, 0));
+                        xDotDot.add(j,0,1*acceleration[0]-0.1*xDot.get(j+1,0));
                     }
                     kernel1.updateStructure(kernel1.getDisplacementVector());
                     t += delta_t;
