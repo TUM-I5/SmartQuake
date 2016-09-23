d6b19d3782e5f27c2c582d1563b6144b4f716766
Felix Wechsler <felix.wechsler@tum.de>
2016-09-23 13:30:13 +0200

    new structure inside kernel2. It's awesome nowgit add app/src/main/java/de/ferienakademie/smartquake/kernel2/Solver.java

---
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/ExplicitSolver.java
@@ -0,0 +1,25 @@
+package de.ferienakademie.smartquake.kernel2;
+
+import org.ejml.data.DenseMatrix64F;
+
+import de.ferienakademie.smartquake.kernel1.Kernel1;
+
+/**
+ * Created by Felix Wechsler on 23/09/16.
+ */
+public class ExplicitSolver extends Solver {
+
+
+    public ExplicitSolver(Kernel1 k1, DenseMatrix64F xDot) {
+        super(k1, xDot);
+    }
+
+
+    public void getAcceleration() {
+        double[] acceleration = k1.getAccelerationProvider().getAcceleration();
+        for (int j = 6; j < k1.getNumDOF(); j += 3) {
+            xDotDot.set(j, 0, 2000 * acceleration[0] - 5 * xDot.get(j, 0) - 100 * k1.getDisplacementVector().get(j, 0));
+            xDotDot.set(j + 1, 0, 2000 * acceleration[1] - 5 * xDot.get(j + 1, 0) - 100 * k1.getDisplacementVector().get(j + 1, 0));
+        }
+    }
+}
---
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/Solver.java
@@ -0,0 +1,47 @@
+package de.ferienakademie.smartquake.kernel2;
+
+import org.ejml.data.DenseMatrix64F;
+
+import de.ferienakademie.smartquake.kernel1.Kernel1;
+
+/**
+ * Created by Felix Wechsler on 23/09/16.
+ */
+public class Solver implements TimeIntegrationSolver {
+
+    //vector of acceleration=xDotDot
+    DenseMatrix64F xDotDot;
+    //vector of velocity=xDot
+    DenseMatrix64F xDot;
+    //vector of displacement=x
+    DenseMatrix64F x;
+
+    //Stiffness Matrix
+    DenseMatrix64F K;
+    //Damping Matrix
+    DenseMatrix64F C;
+    //Mass Matrix
+    DenseMatrix64F M;
+
+    //connection to kernel1
+    Kernel1 k1;
+
+
+    public Solver(Kernel1 k1, DenseMatrix64F xDot) {
+        this.k1 = k1;
+
+        this.M = k1.getMassMatrix();
+        this.K = k1.getStiffnessMatrix();
+        this.C = k1.getDampingMatrix();
+        this.x = k1.getDisplacementVector();
+
+        this.xDot = xDot;
+        xDotDot = new DenseMatrix64F(k1.getNumDOF(), 1);
+        xDotDot.zero();
+    }
+
+
+    public void nextStep(double t, double deltaT) {
+        //will be overwritten in the subclasses
+    }
+}
