12ea55e653f9072bb91a63755c1455e31632c119
Zan <zain.hassan@tum.de>
2016-09-23 11:43:09 +0200

    Material properties protected

--- app/src/main/java/de/ferienakademie/smartquake/model/Material.java
+++ app/src/main/java/de/ferienakademie/smartquake/model/Material.java
@@ -4,16 +4,16 @@ package de.ferienakademie.smartquake.model;
  * Created by alex on 21.09.16.
  */
 public class Material {
-    private double E = 0;   //Young's modulus
-    private double A = 0;   //cross section
-    private double I = 0;   //moment of inertia
-    private double EA = 0;  //
-    private double EI = 0;  //rigidity
-    private double h = 0;   //height of beam (input)
-    private double b = 0;   //width of beam (input)
+    protected double E = 0;   //Young's modulus
+    protected double A = 0;   //cross section
+    protected double I = 0;   //moment of inertia
+    protected double EA = 0;  //
+    protected double EI = 0;  //rigidity
+    protected double h = 0;   //height of beam (input)
+    protected double b = 0;   //width of beam (input)
 
-    private double rho = 0;
-    private double alpha = 0;   //alpha for mass matrix
+    protected double rho = 0;
+    protected double alpha = 0;   //alpha for mass matrix
     //may have to change zeroes
 
     //constructor
@@ -59,45 +59,25 @@ public class Material {
         EI = E*I; //update
     }
 
-    public void setA(double a) {
-        A = a;
-    }
+    public void setA(double a) {  A = a; }
 
-    public double getI() {
-        return I;
-    }
+    public void setI(double i) {  I = i; }
 
-    public void setI(double i) {
-        I = i;
-    }
+    public void setEA(double EA) {  this.EA = EA; }
 
-    public double getEA() {
-        return EA;
-    }
-
-    public void setEA(double EA) {
-        this.EA = EA;
-    }
-
-    public double getEI() {
-        return EI;
-    }
-
-    public void setEI(double EI) {
-        this.EI = EI;
-    }
-
-    public double getE() {
-        return E;
-    }
+    public void setEI(double EI) {  this.EI = EI;  }
 
     public void setE(double e) {  E = e;  }
 
-    public double getRho() { return rho; }
-
     public void setRho(double rho)  { this.rho = rho;}
 
-    public double getAlpha() { return alpha;}
+    public void setAlpha(double alpha)  {this.alpha = alpha;}
 
-    public void setAlpha()  {this.alpha = alpha;}
+    /*
+    public double getI() { return I;   }
+    public double getEA() { return EA; }
+    public double getEI() {  return EI;  }
+    public double getRho() { return rho; }
+    public double getAlpha() { return alpha;}
+    */
 }
