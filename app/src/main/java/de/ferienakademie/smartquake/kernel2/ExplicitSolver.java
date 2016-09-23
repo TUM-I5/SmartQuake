87604534074c078cb5b804bf1f1a25fc6016c559
Felix Wechsler <felix.wechsler@tum.de>
2016-09-23 17:59:49 +0200

    inserted real physical model

--- app/src/main/java/de/ferienakademie/smartquake/kernel2/ExplicitSolver.java
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/ExplicitSolver.java
@@ -1,6 +1,9 @@
 package de.ferienakademie.smartquake.kernel2;
 
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.interfaces.linsol.LinearSolver;
+import org.ejml.ops.CommonOps;
 
 import de.ferienakademie.smartquake.kernel1.Kernel1;
 
@@ -8,8 +11,12 @@ import de.ferienakademie.smartquake.kernel1.Kernel1;
  * Created by Felix Wechsler on 23/09/16.
  */
 public class ExplicitSolver extends Solver {
+    //for the direct sensor input
     double[] acceleration;
 
+    DenseMatrix64F tempVector;
+    //to solve M * xDotDot = f(x, xDot, t)
+    LinearSolver<DenseMatrix64F> linearSolverM;
     /**
      *
      * @param k1
@@ -17,6 +24,12 @@ public class ExplicitSolver extends Solver {
      */
     public ExplicitSolver(Kernel1 k1, DenseMatrix64F xDot) {
         super(k1, xDot);
+
+        //sets up fast linear solver
+        linearSolverM = LinearSolverFactory.chol(k1.getNumDOF());
+        linearSolverM.setA(M);
+
+        tempVector = new DenseMatrix64F(k1.getNumDOF(),1);
     }
 
 
@@ -24,10 +37,39 @@ public class ExplicitSolver extends Solver {
      * This method provides for all explicit solver the acceleration of all nodes
      */
     public void getAcceleration() {
-        acceleration = k1.getAccelerationProvider().getAcceleration();
+
+        tempVector = k1.getLoadVector().copy();
+
+        C.zero();
+        K.zero();
+        for (int j = 6; j < k1.getNumDOF(); j += 3) {
+            C.set(j,j,5);
+            C.set(j+1,j+1,5);
+            K.set(j,j,100);
+            K.set(j+1,j+1,100);
+            tempVector.set(j, 0, 2000 * acceleration[0] );
+            tempVector.set(j + 1, 0, 2000 * acceleration[1] );
+        }
+
+
+
+
+        // next two steps calculating this: tempVecotr= tempVector - C*xDot - K*xD
+        // 1.: tempVector = tempVector - C*xDot
+        CommonOps.multAdd(-1, C,xDot,tempVector);
+
+        //2.: tempVector = tempVector - K*x
+        CommonOps.multAdd(-1, K,x,tempVector);
+
+
+        linearSolverM.solve(tempVector, xDotDot);
+
+        //OLDSTUFF JUST LEAVE IT HERE
+       /* acceleration = k1.getAccelerationProvider().getAcceleration();
         for (int j = 6; j < k1.getNumDOF(); j += 3) {
             xDotDot.set(j, 0, 2000 * acceleration[0] - 5 * xDot.get(j, 0) - 100 * k1.getDisplacementVector().get(j, 0));
             xDotDot.set(j + 1, 0, 2000 * acceleration[1] - 5 * xDot.get(j + 1, 0) - 100 * k1.getDisplacementVector().get(j + 1, 0));
         }
+        */
     }
 }
