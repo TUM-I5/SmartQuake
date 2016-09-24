92ddf99eb389cf1ef03a7833ae872f9cbc0124f1
Alexander Mueller <bau89010@stud.uni-stuttgart.de>
2016-09-24 17:56:02 +0200

    delete Testing Stuff

--- app/src/main/java/de/ferienakademie/smartquake/model/StructureFactory.java
+++ app/src/main/java/de/ferienakademie/smartquake/model/StructureFactory.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import de.ferienakademie.smartquake.kernel1.Testing;
-
 public class StructureFactory {
     public static Structure getSimpleHouse() {
         double width = 8;
@@ -13,62 +11,64 @@ public class StructureFactory {
 
         double half = width * 0.5;
 
+        boolean lumped = true; // Make it false for consistent mass matrices!
 
+        List<Double> unode1 = new LinkedList<>();
+        unode1.add(0.0);
+        unode1.add(0.0);
+        unode1.add(0.0);
 
-        Structure cantilever = new Structure();
-        Structure rotcantilever = new Structure();
         Structure structure = new Structure();
-
         Material testMaterial = new Material();
 
         //Kernel1 stuff
-
         List<Integer> DOFnode1 = new LinkedList<>();
         List<Integer> DOFnode2 = new LinkedList<>();
-
+        List<Integer> DOFnode3 = new LinkedList<>();
+        List<Integer> DOFnode4 = new LinkedList<>();
+        List<Integer> DOFnode5 = new LinkedList<>();
 
         DOFnode1.add(0); //constraint
         DOFnode1.add(1);//constraint
         DOFnode1.add(2);//constraint
 
+        DOFnode2.add(3);//constraint
+        DOFnode2.add(4);//constraint
+        DOFnode2.add(5);//constraint
 
+        DOFnode3.add(6);
+        DOFnode3.add(7);
+        DOFnode3.add(8);
 
-        DOFnode2.add(3);
-
-        DOFnode2.add(4);
-
-        DOFnode2.add(5);
+        DOFnode4.add(9);
+        DOFnode4.add(10);
+        DOFnode4.add(11);
+        DOFnode5.add(12);
+        DOFnode5.add(13);
+        DOFnode5.add(14);
 
         Node n1 = new Node(0, height, DOFnode1);
-        Node n2 = new Node(0, height-2, DOFnode2);
+        Node n2 = new Node(width, height, DOFnode2);
+        Node n3 = new Node(width, height - half, DOFnode3);
+        Node n4 = new Node(0, height - half, DOFnode4);
+        Node n5 = new Node(half, height - 2 * half, DOFnode5);
 
-        //rotated cantilever node
-       // Node n3 = new Node(1,height - half, DOFnode2);
+        Beam b1 = new Beam(n1, n2, testMaterial,lumped);
+        Beam b2 = new Beam(n2, n3, testMaterial,lumped);
+        Beam b3 = new Beam(n3, n4, testMaterial,lumped);
+        Beam b4 = new Beam(n4, n1, testMaterial,lumped);
+        Beam b5 = new Beam(n4, n5, testMaterial,lumped);
+        Beam b6 = new Beam(n5, n3, testMaterial,lumped);
 
-        Beam b1 = new Beam(n1, n2, testMaterial,false);
-        //Beam b2 = new Beam(n1, n3, testMaterial,true);
-
-
-
-        cantilever.addNodes(n1, n2);
-        cantilever.addBeams(b1);
-
-       // rotcantilever.addNodes(n1,n3);
-      //  rotcantilever.addBeam(b2);
+        structure.addNodes(n1, n2, n3, n4, n5);
+        structure.addBeams(b1, b2, b3, b4, b5, b6);
 
         List<Integer> condof= new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             condof.add(i);
         }
-        condof.add(4);
-        //condof.add(5);
 
-        cantilever.setConDOF(condof);
-       // rotcantilever.setConDOF(condof);
-
-        Testing.cantiLeverStaticTest(cantilever);
-
-        //Testing.rotatedcantiLeverStaticTest(rotcantilever);
+        structure.setConDOF(condof);
 
         return structure;
     }
