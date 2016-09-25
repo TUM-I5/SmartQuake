550f522ce0a0d6c245f90d9237e7ae5606f687da
Simon <s.griebel@tum.de>
2016-09-25 20:23:18 +0200

    small comment fix

--- app/src/main/java/de/ferienakademie/smartquake/activity/SimulationActivity.java
+++ app/src/main/java/de/ferienakademie/smartquake/activity/SimulationActivity.java
@@ -135,9 +135,9 @@ public class SimulationActivity extends AppCompatActivity implements Simulation.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulation);
 
-        //mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mExcitationManager = new ExcitationManager();
 
         simFab = (FloatingActionButton) findViewById(R.id.simFab);
