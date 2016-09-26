4ed3e7c6894b4fca71df0c79b06322abd00ef40c
David Schneller <david.schneller@tum.de>
2016-09-26 23:00:23 +0200

    More predefined earthquake data.

--- scripts/predefined-earthquakes/eq-station-find.py
+++ scripts/predefined-earthquakes/eq-station-find.py
@@ -2,29 +2,17 @@ from obspy.clients.fdsn import Client
 from obspy import UTCDateTime
 from obspy.geodetics import locations2degrees
 from math import sqrt
+from eqh import *
 import sys
 
-# Help methods (TODO place them into a separate file)
-def printStatus(msg):
-    print(msg, file=sys.stderr)
-
-def printError(msg):
-    printStatus(msg)
-    sys.exit(1)
-
-def assertError(cond, msg):
-    if not cond:
-        printError(msg)
-
-assertError(len(sys.argv) == 5 or len(sys.argv) == 4, 'Usage: python eq-station-find.py <latitude> <longitude> <altitude> [host]')
+assertError(len(sys.argv) == 3 or len(sys.argv) == 4, 'Usage: python eq-station-find.py <latitude> <longitude> [host]')
 
 host = 'IRIS'
-if len(sys.argv) == 5:
-    host = sys.argv[4].strip()
+if len(sys.argv) == 4:
+    host = sys.argv[3].strip()
 
 lat = float(sys.argv[1])
 lon = float(sys.argv[2])
-alt = float(sys.argv[3])
 
 printStatus('Connecting to %s...' % host)
 client = Client(host)
@@ -39,13 +27,11 @@ stationList = []
 i = 0
 for network in metadata.networks:
     for station in network.stations:
-        flatDistance = locations2degrees(lat, lon, station.latitude, station.longitude)
-        heightDiff = (station.elevation - alt) * .001
-        distance = sqrt(flatDistance * flatDistance + heightDiff * heightDiff)
+        distance = locations2degrees(lat, lon, station.latitude, station.longitude)
         stationList += [(distance, i, network, station)]
         i += 1
 
 stationList.sort()
 
-for x in stationList:
+for x in stationList[0:100]:
     print('%4.2f: %4s.%3s; %3.4f %3.4f %3.4f' % (x[0], x[2].code, x[3].code, x[3].latitude, x[3].longitude, x[3].elevation))
