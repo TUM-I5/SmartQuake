964ea4af19790bbd3a1c018a00958b7bc77b300d
Simon <s.griebel@tum.de>
2016-09-26 17:07:23 +0200

    storing gravitation

--- app/src/main/java/de/ferienakademie/smartquake/excitation/FileAccelerationProvider.java
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/FileAccelerationProvider.java
@@ -6,10 +6,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import de.ferienakademie.smartquake.Simulation;
+import de.ferienakademie.smartquake.activity.SimulationActivity;
+
 /**
  * Created by David Schneller on 25.09.2016.
  */
 public class FileAccelerationProvider extends StoredAccelerationProvider {
+    SimulationActivity activity;
+
+    public FileAccelerationProvider(SimulationActivity activity){
+        super();
+        this.activity = activity;
+    }
+
     /**
      * Load acceleration data from a file
      * @param inputStream stream that passes readings from a file to excitation manager
@@ -25,11 +35,13 @@ public class FileAccelerationProvider extends StoredAccelerationProvider {
         bufferedReader = new BufferedReader(inputStreamReader);
         readingString = bufferedReader.readLine();
         while (readingString != null) {
-            readStringSplit = readingString.split(" ");
+            readStringSplit = readingString.split(";");
 
             currentReading.timestamp = Long.parseLong(readStringSplit[0]);
             currentReading.xAcceleration = Double.parseDouble(readStringSplit[1]);
             currentReading.yAcceleration = Double.parseDouble(readStringSplit[2]);
+            currentReading.yGravity = Double.parseDouble(readStringSplit[3]);
+            currentReading.yGravity = Double.parseDouble(readStringSplit[4]);
 
             readings.add(new AccelData(currentReading));
             readingString = bufferedReader.readLine();
