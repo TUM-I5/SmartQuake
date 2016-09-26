70c4b1f5ba7232dd9ff09ae92e3d204262fb5cc8
Simon <s.griebel@tum.de>
2016-09-26 10:43:56 +0200

    added classes for providing gravity

---
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/GravityProvider.java
@@ -0,0 +1,8 @@
+package de.ferienakademie.smartquake.excitation;
+
+/**
+ * Created by simon on 26.09.16.
+ */
+public abstract class GravityProvider {
+    public abstract void getGravity();
+}
---
+++ app/src/main/java/de/ferienakademie/smartquake/excitation/SensorGravityProvider.java
@@ -0,0 +1,11 @@
+package de.ferienakademie.smartquake.excitation;
+
+/**
+ * Created by simon on 26.09.16.
+ */
+public class SensorGravityProvider extends GravityProvider {
+    @Override
+    public void getGravity() {
+
+    }
+}
