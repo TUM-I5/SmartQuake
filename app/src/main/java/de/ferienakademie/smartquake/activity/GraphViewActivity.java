380659511247a4a005f8c0a33e161bb2c83afb9e
geldnens <ngeldner3@gmail.com>
2016-09-27 12:01:30 +0200

    Add: Menu to select DOFs to view

--- app/src/main/java/de/ferienakademie/smartquake/activity/GraphViewActivity.java
+++ app/src/main/java/de/ferienakademie/smartquake/activity/GraphViewActivity.java
@@ -2,10 +2,17 @@ package de.ferienakademie.smartquake.activity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ListView;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -24,7 +31,7 @@ import de.ferienakademie.smartquake.view.CanvasView;
 
 public class GraphViewActivity extends AppCompatActivity {
     private LineChart nodeDataChart;
-    private ListView dofCheckboxList;
+    private int numRotationalDofs = 1;
     private CanvasView nodeView;
     private CanvasView modelView;
 
@@ -34,22 +41,9 @@ public class GraphViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_graph_view);
         nodeDataChart = (LineChart) findViewById(R.id.nodeDataChart);
         nodeDataChart.setBackgroundColor(Color.WHITE);
-        nodeDataChart.setDescription("Dummy displacement");
+        nodeDataChart.setDescription("Node displacements");
         nodeDataChart.setPinchZoom(false);
 
-        /*List<String> dofs = new ArrayList<>();
-        dofs.add("Some DOF");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item_checkbox, R.id.dofCheckBox, dofs);
-        dofCheckboxList = (ListView) findViewById(R.id.dofChoiceList);
-        dofCheckboxList.setAdapter(adapter);
-        dofCheckboxList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
-            }
-        });*/
 
         List<String> xList = new ArrayList<>();
         List<Entry> line1 = new ArrayList<>();
@@ -62,8 +56,8 @@ public class GraphViewActivity extends AppCompatActivity {
         }
 
         List<ILineDataSet> sets = new ArrayList<>();
-        sets.add(new LineDataSet(line1, "values 1"));
-        sets.add(new LineDataSet(line2, "values 2"));
+        sets.add(new LineDataSet(line1, "X axis (dummy)"));
+        sets.add(new LineDataSet(line2, "Y axis (dummy)"));
 
         for (ILineDataSet iSet : sets) {
             LineDataSet set = (LineDataSet) iSet;
@@ -88,8 +82,21 @@ public class GraphViewActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.actions_start_activity, menu);
+        getMenuInflater().inflate(R.menu.actions_graph_view, menu);
+        //onPrepareOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        for (int i = 0; i < numRotationalDofs; ++i) {
+            if (menu.findItem(i) == null) {
+                menu.add(R.id.dofCheckboxGroup, i, i + 2, "View rotation " + Integer.toString(i));
+                MenuItem newItem = menu.findItem(i);
+                newItem.setCheckable(true);
+                newItem.setChecked(false);
+            }
+        }
         return true;
     }
 
@@ -97,9 +104,23 @@ public class GraphViewActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.xDofCheckbox:
+            case R.id.yDofCheckbox:
+                // why is this necessary again?
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                break;
+            default:
+                break;
+        }
+
+        if (0 <= id  && id < numRotationalDofs) {
+            if (item.isChecked()) item.setChecked(false);
+            else item.setChecked(true);
         }
 
         return super.onOptionsItemSelected(item);
