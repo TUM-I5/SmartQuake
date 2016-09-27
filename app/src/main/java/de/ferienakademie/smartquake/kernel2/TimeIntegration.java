a4a18cb966a43b4feb90c27f627b14e8a264806d
Felix Wechsler <felix.wechsler@tum.de>
2016-09-27 09:38:20 +0200

    code commenting

--- app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
+++ app/src/main/java/de/ferienakademie/smartquake/kernel2/TimeIntegration.java
@@ -1,20 +1,28 @@
 package de.ferienakademie.smartquake.kernel2;
 
+import android.util.Log;
+
 import org.ejml.data.DenseMatrix64F;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+
 
 import de.ferienakademie.smartquake.excitation.AccelerationProvider;
 import de.ferienakademie.smartquake.kernel1.SpatialDiscretization;
 import de.ferienakademie.smartquake.managers.PreferenceReader;
 
 /**
+ * This is the main class for the simulation.
+ * The time integration is here managed.
  * Created by Felix Wechsler on 21/09/16.
  */
 public class TimeIntegration {
 
+    //object to obtain matrices
     SpatialDiscretization spatialDiscretization;
+    //object to get sensor data
     AccelerationProvider accelerationProvider;
 
     // total computed time between every time step. This variable prevents computing more than GUI wants
@@ -34,12 +42,13 @@ public class TimeIntegration {
     ExecutorService executorService;
 
 
-
     /**
-    * @param spatialDiscretization
-    *          object to obtain all matrices, displacements, external forces
-    *
-    **/
+     *
+     * @param spatialDiscretization
+     *        object to get matrices
+     * @param accelerationProvider
+     *        object to get accelerations from Sensors
+     */
     public TimeIntegration(SpatialDiscretization spatialDiscretization, AccelerationProvider accelerationProvider) {
         this.spatialDiscretization = spatialDiscretization;
         this.accelerationProvider = accelerationProvider;
@@ -85,6 +94,8 @@ public class TimeIntegration {
      */
     public class SimulationStep {
 
+        //variable which can stop the simulation
+        //it will be set to false if the calculation of the displacements takes longer than than
         boolean isRunning;
 
         public SimulationStep execute() {
@@ -95,33 +106,31 @@ public class TimeIntegration {
                     //reset time
                     t = 0;
 
-                    //calculates time step
-
                     //update loadVector
                     spatialDiscretization.updateLoadVector(accelerationProvider.getAcceleration());
 
                     //get the loadVector for the whole calculation
                     solver.setFLoad(spatialDiscretization.getLoadVector());
 
-
-
-
                     //long firstTime = System.nanoTime();
+                    //this loop performs the calculation
+                    //it calculates one frame then it stops
                     while(t < 0.03-0.000001 && isRunning) {
                         //calculate new displacement
                         solver.nextStep(t, delta_t);
                         //add ground movement for recording
                         solver.setGroundPosition(delta_t);
+
                         t += delta_t;
 
                     }
 
-                    //for the sensor team
+                    //for the sensor team the global time since beginnig
                     globalTime += 0.03;
 
-                    //for recording
+                    //for checking the calculation time
                     //long secondTime = System.nanoTime();
-                  //  Log.e("Timestamp",""+(secondTime-firstTime));
+                    //Log.e("Timestamp",""+(secondTime-firstTime));
 
                     //update the displacement in the node variables
                     spatialDiscretization.updateDisplacementsOfStructure(solver.getX(), solver.getGroundPosition());
