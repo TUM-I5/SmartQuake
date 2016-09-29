080b58c1db857d91b7edd5729fc3e657e5798cb0
David Schneller <david.schneller@tum.de>
2016-09-29 08:50:56 +0200

    Some changes to node display.

--- app/src/main/java/de/ferienakademie/smartquake/model/Node.java
+++ app/src/main/java/de/ferienakademie/smartquake/model/Node.java
@@ -136,13 +136,10 @@ public class Node {
 
 
     public double getRadius() {
-        //If you want to know "why this formula?"
-        //Well, there's no real reason for this one! Yep.
-        //But ok, let's go into detail: normally it is like r² ~ A, so we take the sqrt.
-        //Then some scaling b/c alone it would be probably too large. Then we simply add a logarithmic factor to reduce scaling even more.
-        //And finally, we add the 0.05 which every mass should have.
-        //And that's it. If you got questions or do not like this formula, call me.
-        return Math.log10(Math.sqrt(nodeMass) * .001 + 1) + 0.05;
+        //Let's go into detail: normally it is like r² ~ A, so we take the sqrt.
+        //Then some scaling b/c alone it would be probably too large. After that we simply add a logarithmic factor to reduce scaling even more.
+        //And finally, we add the 0.05 which the zero mass had before. And that's it!
+        return Math.log10(Math.sqrt(nodeMass) * .1 + 1) + 0.05;
     }
 
     public void clearBeams() {
